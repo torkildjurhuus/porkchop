33d3da8793da63ef7830b4cce004a4fe164e3f46
Torkil Djurhuus <torkil.djurhuus@globetracker.com>
2021-06-17 10:53:52 +0100

    designbroytingar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4 className="Header-title">skal eg eta kotelett i dag</h4>
+        <h4 className="Header-title">Er tað kotelettdagur í dag?</h4>
         <img src={porkchop} className="App-logo" alt="logo" />
         <p>
           <b className="Header-title">{day === 4 ? "JA! 😍" : "Nei 😭"}</b>
