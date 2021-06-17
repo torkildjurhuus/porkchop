46fe57f98bae5e6c8d571ec202e90eeb7bc6fac5
Torkil Djurhuus <torkil.djurhuus@globetracker.com>
2021-06-17 10:45:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import porkchop from "./img/porkchop.png";
+import "./App.css";
+
+var day = new Date().getDay();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h4 className="Header-title">skal eg eta kotelett i dag</h4>
+        <img src={porkchop} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b className="Header-title">{day === 4 ? "JA! 😍" : "Nei 😭"}</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
